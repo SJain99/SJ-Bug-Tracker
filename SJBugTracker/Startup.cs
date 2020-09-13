5cd30067722406df60bcb590e19f9916f7476009
Saransh Jain <saransh.jain@mail.utoronto.ca>
2020-09-12 22:24:02 -0400

    Add project files.

---
+++ SJBugTracker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SJBugTracker.Startup))]
+namespace SJBugTracker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
