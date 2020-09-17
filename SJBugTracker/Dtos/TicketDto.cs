b808f98709a096c64ed9d342ffd76eeb10baf609
Saransh Jain <saransh.jain@mail.utoronto.ca>
2020-09-16 22:38:56 -0400

    Create DTOs for Project and Ticket.

---
+++ SJBugTracker/Dtos/TicketDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SJBugTracker.Dtos
+{
+    public class TicketDto
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public ProjectDto ProjectDto { get; set; }
+    }
+}
