954967855b1b61661f6fa3352cf0c728e50286e0
Saransh Jain <saransh.jain@mail.utoronto.ca>
2020-09-16 22:58:43 -0400

    Install and initialize Automapper plugin.

---
+++ SJBugTracker/App_Start/MappingProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using SJBugTracker.Dtos;
+using SJBugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SJBugTracker.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            Mapper.CreateMap<Project, ProjectDto>();
+            Mapper.CreateMap<ProjectDto, Project>();
+            Mapper.CreateMap<Ticket, TicketDto>();
+            Mapper.CreateMap<TicketDto, Ticket>();
+        }
+    }
+}
