671b49ff28675df9a9a33e0b859f6a68333a5b39
Saransh Jain <saransh.jain@mail.utoronto.ca>
2020-10-24 15:28:58 -0400

    Create many-to-many relationship between ApplicationUser and Project.

---
+++ SJBugTracker/Migrations/202010241924047_ApplicationUserProjectManyToMany.cs
@@ -0,0 +1,34 @@
+namespace SJBugTracker.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ApplicationUserProjectManyToMany : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ApplicationUserProjects",
+                c => new
+                    {
+                        ApplicationUser_Id = c.String(nullable: false, maxLength: 128),
+                        Project_Id = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => new { t.ApplicationUser_Id, t.Project_Id })
+                .ForeignKey("dbo.AspNetUsers", t => t.ApplicationUser_Id, cascadeDelete: true)
+                .ForeignKey("dbo.Projects", t => t.Project_Id, cascadeDelete: true)
+                .Index(t => t.ApplicationUser_Id)
+                .Index(t => t.Project_Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.ApplicationUserProjects", "Project_Id", "dbo.Projects");
+            DropForeignKey("dbo.ApplicationUserProjects", "ApplicationUser_Id", "dbo.AspNetUsers");
+            DropIndex("dbo.ApplicationUserProjects", new[] { "Project_Id" });
+            DropIndex("dbo.ApplicationUserProjects", new[] { "ApplicationUser_Id" });
+            DropTable("dbo.ApplicationUserProjects");
+        }
+    }
+}
--- SJBugTracker/Models/Project.cs
+++ SJBugTracker/Models/Project.cs
@@ -14,5 +14,6 @@ namespace SJBugTracker.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public List<Ticket> Tickets { get; set; }
+        public virtual ICollection<ApplicationUser> Users { get; set; }
     }
 }
