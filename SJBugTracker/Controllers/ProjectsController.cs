f21f8eee0b6c6ef1de9b6349bbc32753f9f3f12f
Saransh Jain <saransh.jain@mail.utoronto.ca>
2021-01-13 20:17:58 -0500

    Create project user management functionality.

--- SJBugTracker/Controllers/ProjectsController.cs
+++ SJBugTracker/Controllers/ProjectsController.cs
@@ -1,8 +1,11 @@
-﻿using SJBugTracker.Models;
+﻿using Microsoft.AspNet.Identity.Owin;
+using SJBugTracker.Models;
 using SJBugTracker.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
 
@@ -16,6 +19,7 @@ namespace SJBugTracker.Controllers
         {
             _context = new ApplicationDbContext();
         }
+
         public ActionResult Index()
         {
             return View();
@@ -60,6 +64,80 @@ namespace SJBugTracker.Controllers
             };
 
             return View(viewModel);
+        }
+
+        public ActionResult UserManagement(int id)
+        {
+            var actions = new List<string>
+            {
+                "Assign",
+                "Remove"
+            };
+
+            var users = _context.Users.Include(u => u.Roles).Include(u => u.Projects).ToList();
+
+            foreach(var user in users.ToList())
+            {
+                foreach(var role in user.Roles)
+                {
+                    if(role.RoleId == "759a9587-8b75-4209-a5eb-3f4e57e3a3fc" || role.RoleId == "6b8ce7f4-726d-4fc7-8fd9-2e4f09b9e066")
+                    {
+                        users.Remove(user);
+                    }
+                }
+            }
+
+            var viewModel = new ProjectAssignmentViewModel
+            {
+                Project = _context.Projects.SingleOrDefault(p => p.Id == id),
+                Users = users,
+                Roles = _context.Roles.ToList(),
+                Actions = actions
+            };
+
+            return View(viewModel);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public RedirectToRouteResult ManageProject(ProjectAssignmentViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+                return RedirectToAction("Index");
+
+            if(viewModel.Action == "Assign")
+            {
+                AssignProjectToUsers(viewModel);
+                return RedirectToAction("Index");
+            } else
+            {
+                RemoveProjectFromUsers(viewModel);
+                return RedirectToAction("Index");
+            }
+        }
+
+        public void AssignProjectToUsers(ProjectAssignmentViewModel viewModel)
+        {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == viewModel.ProjectId);
+            foreach(var userID in viewModel.SelectedUsers)
+            {
+                var user = _context.Users.SingleOrDefault(u => u.Id == userID);
+                if(!user.Projects.Contains(project))
+                    user.Projects.Add(project);
+                _context.SaveChanges();
+            }
+        }
+
+        public void RemoveProjectFromUsers(ProjectAssignmentViewModel viewModel)
+        {
+            var project = _context.Projects.SingleOrDefault(p => p.Id == viewModel.ProjectId);
+            foreach (var userID in viewModel.SelectedUsers)
+            {
+                var user = _context.Users.SingleOrDefault(u => u.Id == userID);
+                if (user.Projects.Contains(project))
+                    user.Projects.Remove(project);
+                _context.SaveChanges();
+            }
         }
     }
 }
---
+++ SJBugTracker/ViewModels/ProjectAssignmentViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using SJBugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SJBugTracker.ViewModels
+{
+    public class ProjectAssignmentViewModel
+    {
+        public Project Project { get; set; }
+        public List<ApplicationUser> Users { get; set; }
+        [Display(Name = "Users")]
+        public List<string> SelectedUsers { get; set; }
+        public List<IdentityRole> Roles { get; set; }
+        public int ProjectId { get; set; }
+        public List<string> Actions { get; set; }
+        public string Action { get; set; }
+    }
+}
