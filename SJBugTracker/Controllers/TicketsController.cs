d8b5782fcd451737534ab326ea0faac52987f2b1
Saransh Jain <saransh.jain@mail.utoronto.ca>
2021-01-23 19:54:12 -0500

    Create ticket user management functionality.

--- SJBugTracker/Controllers/TicketsController.cs
+++ SJBugTracker/Controllers/TicketsController.cs
@@ -2,6 +2,7 @@
 using SJBugTracker.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -50,6 +51,93 @@ namespace SJBugTracker.Controllers
             };
 
             return View(viewModel);
+        }
+
+        public ActionResult UserManagement(int id)
+        {
+            var actions = new List<string>
+            {
+                "Assign",
+                "Remove"
+            };
+
+            var ticket = _context.Tickets.SingleOrDefault(t => t.Id == id);
+            var users = _context.Users.Include(u => u.Roles).Include(u => u.Projects).ToList();
+
+            foreach (var user in users.ToList())
+            {
+                if (user.Roles.FirstOrDefault().RoleId != "dc581abb-7c4a-4800-8e50-7b65d0e764aa")
+                    users.Remove(user);
+
+                if (user.Projects != null)
+                {
+                    var projectCount = user.Projects.Count();
+                    var projectIndex = 0;
+
+                    foreach (var project in user.Projects)
+                    {
+                        if (project.Id != ticket.ProjectId)
+                            projectIndex++;
+                    }
+
+                    if (projectIndex == projectCount)
+                        users.Remove(user);
+                }
+                else
+                    users.Remove(user);
+            }
+
+            var viewModel = new TicketAssignmentViewModel
+            {
+                Ticket = ticket,
+                Users = users,
+                Actions = actions
+            };
+
+            return View(viewModel);
+        }
+
+        [ValidateAntiForgeryToken]
+        [HttpPost]
+        public RedirectToRouteResult ManageTicket(TicketAssignmentViewModel viewModel)
+        {
+            if (!ModelState.IsValid)
+                return RedirectToAction("Index");
+
+            if (viewModel.Action == "Assign")
+            {
+                AssignTicketToUsers(viewModel);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                RemoveTicketFromUsers(viewModel);
+                return RedirectToAction("Index");
+            }
+        }
+
+        public void AssignTicketToUsers(TicketAssignmentViewModel viewModel)
+        {
+            var ticket = _context.Tickets.SingleOrDefault(t => t.Id == viewModel.TicketId);
+            foreach (var userID in viewModel.SelectedUsers)
+            {
+                var user = _context.Users.SingleOrDefault(u => u.Id == userID);
+                if (!user.Tickets.Contains(ticket))
+                    user.Tickets.Add(ticket);
+                _context.SaveChanges();
+            }
+        }
+
+        public void RemoveTicketFromUsers(TicketAssignmentViewModel viewModel)
+        {
+            var ticket = _context.Tickets.SingleOrDefault(t => t.Id == viewModel.TicketId);
+            foreach (var userID in viewModel.SelectedUsers)
+            {
+                var user = _context.Users.SingleOrDefault(u => u.Id == userID);
+                if (user.Tickets.Contains(ticket))
+                    user.Tickets.Remove(ticket);
+                _context.SaveChanges();
+            }
         }
     }
 }
---
+++ SJBugTracker/ViewModels/TicketAssignmentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using SJBugTracker.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SJBugTracker.ViewModels
+{
+    public class TicketAssignmentViewModel
+    {
+        public Ticket Ticket { get; set; }
+        public List<ApplicationUser> Users { get; set; }
+        [Display(Name = "Users")]
+        public List<string> SelectedUsers { get; set; }
+        public int TicketId { get; set; }
+        public List<string> Actions { get; set; }
+        public string Action { get; set; }
+    }
+}
